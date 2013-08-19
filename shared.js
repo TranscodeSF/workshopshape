63b1e0c60df309e779f724976f76d2c449c2842a
Naomi Seyfer <naomi@seyfer.org>
2013-08-18 19:10:14 -0700

    Actually I misspelled arias name

--- shared.js
+++ shared.js
@@ -3,5 +3,5 @@ userIsAdmin = function () {
   var user = Meteor.user();
   if (!user) return false;
   var username = user.services && user.services.github && user.services.github.username;
-  return _.contains(["ayust", "ariaBennet", "sixolet"], username); // XXX: HARDCODE!!!
+  return _.contains(["ayust", "ariaBennett", "sixolet"], username); // XXX: HARDCODE!!!
 };
